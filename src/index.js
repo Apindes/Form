09b573278d92f49fa9e81aff68e4854daf783b80
Apindes <apindesign@yahoo.com>
2018-10-01 13:52:10 +0300

    creation

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const arr  = [1,3,66];
+const customFunc = () => console.log("logging...");
+window.customFunc = customFunc;
+
+import Form from "./App";
+import style from "./main.css";
+import style2 from "./main.scss"
